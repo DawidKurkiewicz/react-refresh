5d477c22d59b318147d2d74c162e7d93f4dbf21a
Kurkiewicz <dawidkurkiewicz516@gmail.com>
2019-09-10 09:47:44 +0200

    change person component from function to class

--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./Person.css"
-const person = (props) => {
-    return (
-        <div className="Person">
-            <button onClick={props.delete}>delete</button>
-            <h3 onClick={props.click}>i'm a {props.name} and i am {props.age} years old</h3>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+class Person extends Component {
+    render() {
+        return (
+            <div className="Person">
+                <button onClick={this.props.delete}>delete</button>
+                <h3 onClick={this.props.click}>i'm a {this.props.name} and i am {this.props.age} years old</h3>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
 }
-
-export default person
+export default Person
