bd8e7ad73ef9ee7935752c4d0c605786928a8c57
Kurkiewicz <dawidkurkiewicz516@gmail.com>
2019-09-13 10:05:43 +0200

    added aux component

--- src/Person.js
+++ src/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import "./Person.css"
-import Aux from "./Aux"
+import Aux from "./Auxiliary"
 class Person extends Component {
     render() {
         return (
