c2400a96805909ca812aca3bcbffdb8958c5beda
Kurkiewicz <dawidkurkiewicz516@gmail.com>
2019-09-19 10:00:55 +0200

    unmutable state, changed styles

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ class App extends Component {
     showPersons: false,
     showUsers: false,
     showInput: false,
-    showCockpit: false
+    showCockpit: false,
+    isBlockPersons:false,
+    isBlockUsers:false,
+    isBlockInput:false,
   }
   inputChangedHanlder = (event) => {
     this.setState({ userInput: event.target.value })
@@ -85,15 +88,16 @@ class App extends Component {
   }
   togglePersonsHandler = () => {
     const doesShowPersons = this.state.showPersons;
-    this.setState({ showPersons: !doesShowPersons })
+    this.setState({ showPersons: !doesShowPersons, isBlockPersons: !this.state.isBlockPersons })
+
   }
   toggleUsersHandler = () => {
     const doesShowUsers = this.state.showUsers;
-    this.setState({ showUsers: !doesShowUsers })
+    this.setState({ showUsers: !doesShowUsers, isBlockUsers: !this.state.isBlockUsers })
   }
   toggleInputHandler = () => {
     const doesShowInput = this.state.showInput;
-    this.setState({ showInput: !doesShowInput })
+    this.setState({ showInput: !doesShowInput, isBlockInput: !this.state.isBlockInput })
   }
   toggleCockpitHandler = () => {
     const doesShowCockpit = this.state.showCockpit;
@@ -103,29 +107,32 @@ class App extends Component {
     const doesShowWithClass = this.state.showWithClass
     this.setState({ showWithClass: !doesShowWithClass })
   }
-  personInitialState=()=>{
-    this.setState({persons: [
-      {
-        name: "Max", age: 24, id: "0"
-      },
-      {
-        name: "Manuel", age: 25, id: "1"
-      },
-      {
-        name: "Marc", age: 27, id: "2"
-      }
-    ],})
+  personInitialState = () => {
+    this.setState({
+      persons: [
+        {
+          name: "Max", age: 24, id: "0"
+        },
+        {
+          name: "Manuel", age: 25, id: "1"
+        },
+        {
+          name: "Marc", age: 27, id: "2"
+        }
+      ],
+    })
   }
-  userInitialState=()=>{
-    this.setState({ users: [
-      {
-        userName: "Mike", id: "3"
-      },
-      {
-        userName: "Linda", id: "4"
-      }
-    ]
-  })
+  userInitialState = () => {
+    this.setState({
+      users: [
+        {
+          userName: "Mike", id: "3"
+        },
+        {
+          userName: "Linda", id: "4"
+        }
+      ]
+    })
   }
   render() {
     const charList = this.state.userInput.split('').map((char, index) => {
@@ -156,20 +163,30 @@ class App extends Component {
       display: "block",
       margin: "10px"
     };
+    const styleWithColorAndMargin = {
+      backgroundColor: "LightCoral",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      color: "white",
+      display: "block",
+      margin: "10px auto"
+    };
     let users = null;
     if (this.state.showUsers) {
       users = (
         <div className="Wrapper">
-          <div style={{display:"flex",justifyContent:"center"}}>
-          <button style={styleWithoutMargin}
-            onClick={this.userNameChangeHandler}>
-            Switch User Name 
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button style={styleWithoutMargin}
+              onClick={this.userNameChangeHandler}>
+              Switch User Name
           </button>
-          <button style={styleWithoutMargin}
-            onClick={()=>this.userInitialState()}>
-            Reset
+            <button style={styleWithoutMargin}
+              onClick={() => this.userInitialState()}>
+              Reset
        </button>
-       </div>
+          </div>
           <UserInput change={this.inputUserNameChangeHandler}
             currentName={this.state.users[0].userName} />
           {this.state.users.map((user, index) => {
@@ -187,16 +204,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div className="Wrapper">
-          <div style={{display:"flex",justifyContent:"center"}}>
-          <button style={styleWithoutMargin}
-            onClick={() => this.switchNameHandler("Jerry")}>
-            Switch Person Name
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button style={styleWithoutMargin}
+              onClick={() => this.switchNameHandler("Jerry")}>
+              Switch Person Name
        </button>
-       <button style={styleWithoutMargin}
-            onClick={()=>this.personInitialState()}>
-            Reset
+            <button style={styleWithoutMargin}
+              onClick={() => this.personInitialState()}>
+              Reset
        </button>
-       </div>
+          </div>
           {this.state.persons.map((person, index) => {
             return <Person
               key={person.id}
@@ -226,18 +243,18 @@ class App extends Component {
       />
       : null
     let withClass = this.state.showWithClass ?
-      <div >
+      <div>
         <button onClick={this.toggleCockpitHandler} style={style}>Toggle Cockpit</button>
         {cockpit}
-        <div>
-          <button onClick={this.togglePersonsHandler} style={style}>Toggle Persons</button>
+        <div className={(this.state.isBlockPersons || this.state.isBlockUsers || this.state.isBlockInput) ? "Block" : "Center"}>
+          <button onClick={this.togglePersonsHandler} style={(this.state.isBlockPersons || this.state.isBlockUsers || this.state.isBlockInput) ? styleWithColorAndMargin : styleWithoutMargin}>Toggle Persons</button>
           {persons}
-          <button onClick={this.toggleUsersHandler} style={style}>Toggle Users</button>
+          <button onClick={this.toggleUsersHandler} style={(this.state.isBlockPersons || this.state.isBlockUsers || this.state.isBlockInput) ? styleWithColorAndMargin : styleWithoutMargin}>Toggle Users</button>
           {users}
-          <button onClick={this.toggleInputHandler} style={style}>Toggle Input</button>
+          <button onClick={this.toggleInputHandler} style={(this.state.isBlockPersons || this.state.isBlockUsers || this.state.isBlockInput) ? styleWithColorAndMargin : styleWithoutMargin}>Toggle Input</button>
           {input}
         </div>
-       
+
       </div> : null
     return (
       <div>
--- src/CharInput.js
+++ src/CharInput.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Validation from"./Validation"
 const input = (props) => {
-    if(props.par.length >= 1 && document.getElementById("paragraph") ){
-document.getElementById("paragraph").className="Person"   
- }
     return (
         <div className="Wrapper">
              <input type="text" onChange={props.change} value={props.val}/>
-        <p id="paragraph">{props.par}</p>
+        <p className="Person">{props.par}</p>
         <Validation inputLength={props.len}/>
         <div className="Letters">{props.char}</div>
 
